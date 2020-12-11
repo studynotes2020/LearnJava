0acc363e466f5e24866b4157fc1bfe0a7d2db73b
joonyi <joonyi2011@gmail.com>
2020-12-11 10:19:42 -0800

    first commit

---
+++ HelloProgram.java
@@ -0,0 +1,14 @@
+// import acm.graphics.*;
+// import acm.graphics.*;
+
+// public class HelloProgram extends GraphicsProgram{
+//     public void run() {
+//         add(new GLable("hello, world", 100, 75));
+//     }
+// }
+
+public class HelloProgram {
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+    }
+}
